b22163437f0b375e23bf24c4c3ccda6baf88e772
CamilaVelezGiraldo <111664816+velitaz@users.noreply.github.com>
2025-05-01 12:38:55 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -13,7 +13,7 @@ from googletrans import Translator
 st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
-image = Image.open('traductor (1).jpg')
+image = Image.open('traductor (1).png')
 st.image(image, width=300)
 
 with st.sidebar:
